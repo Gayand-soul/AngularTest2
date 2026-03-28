891d6da1e3a1f69dc478f9afb06a96f040d15184
Gaya.A <gaya.andersson@gmail.com>
2026-03-28 14:11:49 +0100

    Implement user input submission and logging
    Added user input handling and logging for security alerts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,18 @@ import { Component, signal } from "@angular/core";
 export class AppComponent {
   name = signal('My Signal!');
   imageURL = signal('assets/Sakura.JPG');
-
+   //metod
   getName(){
     return this.name();
   }
+  
+  //nya ändringar-för lekuppgift9
+  userInput: string = '';
+  savedInput: string = '';
+  submitInput() {
+    this.savedInput = this.userInput;
+    // Simulerad logg för SIEM
+    console.log('⚠️ Security Alert: User input submitted ->', this.userInput);
+  }
+
 }
